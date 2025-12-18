269fcb0212b0c157f2265d8a089dfadad0dd108f
HyuMochi <Hyumochi123@gmail.com>
2025-12-18 12:04:16 +0100

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackground";
+import { Navbar } from "../components/NavBar";
+
+export const Home = () => {
+    return<div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+
+        {/*Theme Toggle*/}
+        <ThemeToggle/>
+
+        {/*background effects*/}
+        <StarBackground/>
+
+
+
+        {/*NavBar*/}
+        <Navbar/>
+
+        {/*MainContent*/}
+
+        {/*Footer*/}
+
+ 
+
+    </div>;
+}
