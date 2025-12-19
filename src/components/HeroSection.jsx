824f4ac588e376f3f7d0d3d5633b9d3d6cb28459
Hieu Phan <hphan.tech@gmail.com>
2025-12-19 15:47:58 +0100

    AboutMe section finished

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,7 +2,7 @@ import { ArrowDown } from "lucide-react"
 
 export const HeroSection = () => {
     return <section id="hero" className="relative min-h-screen flex flex-col items-center justify-center px-4">
-        <div className="container max-w-4xl mx=auto text-center z-10">
+        <div className="container max-w-4xl mx-auto text-center z-10">
             <div className="space-y-6">
                 <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
                     <span className="opacity-0 animate-fade-in"> Hi,</span>
@@ -13,17 +13,18 @@ export const HeroSection = () => {
                 </h1>
                 <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
                     Third-year IT student focused on software engineering, with the goal of becoming a full-stack developer. 
-                    I built this website with React as a space to share my skills, projects, and experience and to give a clear picture of who I am and how I work.
+                    I built this website as a space to share my skills, projects, and experience and to give a clear picture of who I am and how I work.
                 </p>
-                <div className="pt-4 opacity-0 animate-fade-in-delay-4">
+                <div className="pt-4 opacity-0 animate-fade-in-delay-4 flex flex-col sm:flex-row gap-4 pt-4 justify-center">
                     <a href="#projects" className="cosmic-button">
                         view my work
                     </a>
+                
                 </div>
             </div>
         </div>
-        <div className="absolute bottom-8 left-1/2 transform-translate-x-1/2 flex flex-col items-center animate-bounce">
-        <span>Scroll</span>
+        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
+        <span className="text-sm text-muted-foreground mb-2">Scroll</span>
         <ArrowDown className="h-5 w-5 text-primary"/>
 
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/NavBar";
 import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
 export const Home = () => {
     return<div className="min-h-screen bg-background text-foreground overflow-x-hidden">
 
@@ -19,6 +20,7 @@ export const Home = () => {
         {/*MainContent*/}
         <main>
             <HeroSection />
+            <AboutSection />
         </main>
 
         {/*Footer*/}
